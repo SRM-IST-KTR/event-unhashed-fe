ef7abd66291c1c66939c2d9b650a82f8128e51ec
sarthak sachdeva <sarthak.sachdeva.73@gmail.com>
2022-05-27 11:27:56 +0530

    :construction: add redirect links to anchor tags

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,11 +2,15 @@ import FooterIcons from "./footerIcons";
 
 const Footer = () => {
   return (
-    <div className="bg-gradient-to-bl from-[#403998] to-[#3F3F3F] h-40 text-white flex justify-evenly items-center">
-      <div className="text-2xl font-bold">community@githubsrm.tech</div>
-      <div className="text-2xl flex flex-col justify-center items-center">
+    <div className="bg-gradient-to-bl from-[#403998] to-[#3F3F3F] h-40 text-white flex justify-evenly items-center text-2xl">
+      <a href="mailto:community@githubsrm.tech" className="font-bold">
+        community@githubsrm.tech
+      </a>
+      <div className="flex flex-col justify-center items-center">
         <div>Alone we can do so little, together we can do so much.</div>
-        <div className="text-2xl font-bold">Github Community SRM</div>
+        <a href="https://githubsrm.tech/" className="font-bold">
+          Github Community SRM
+        </a>
       </div>
       <FooterIcons />
     </div>
--- src/components/footer/footerIcons.js
+++ src/components/footer/footerIcons.js
@@ -8,10 +8,18 @@ import { MdEmail } from "react-icons/md";
 const FooterIcons = () => {
   return (
     <div className="text-5xl flex w-64 justify-around">
-      <MdEmail />
-      <AiFillTwitterSquare />
-      <AiFillInstagram />
-      <AiFillLinkedin />
+      <a href="mailto:community@githubsrm.tech">
+        <MdEmail />
+      </a>
+      <a href="https://twitter.com/GithubSrm">
+        <AiFillTwitterSquare />
+      </a>
+      <a href="https://www.instagram.com/githubsrm/">
+        <AiFillInstagram />
+      </a>
+      <a href="https://www.linkedin.com/company/githubsrm/mycompany/">
+        <AiFillLinkedin />
+      </a>
     </div>
   );
 };
