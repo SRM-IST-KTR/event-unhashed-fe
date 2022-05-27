67a8464f0643eba3a8fac9a59f4231a233b8087f
sarthak sachdeva <sarthak.sachdeva.73@gmail.com>
2022-05-27 10:18:25 +0530

    :construction: add folder structure for footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Footer from "./components/footer/footer";
 
 function App() {
   return (
     <div className="App">
-      <p className='text-5xl'>Hello</p>
+      <p className="text-5xl">Hello</p>
+      <Footer />
     </div>
   );
 }
