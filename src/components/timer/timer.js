c177c5f6f871298f8cd152c5f7e045bfa4085b9d
Sudhanshu Srivastava <71897832+Codered9@users.noreply.github.com>
2022-05-28 15:00:27 +0530

    ⚡️ Responsive fixes

--- src/components/timer/timer.js
+++ src/components/timer/timer.js
@@ -1,5 +1,11 @@
+import Hero from "../../utils/icons/githubHeroOld.png"
+
 const Timer = () => {
-  return <div>timer</div>;
+  return <div className="flex flex-row mx-12 md:w-6/12 md:mx-auto">
+    <figure className="basis-2/5">
+      <img src={Hero} alt="Github Hero" className=" w-64 h-auto mx-auto" />
+    </figure>
+  </div>;
 };
 
 export default Timer;
