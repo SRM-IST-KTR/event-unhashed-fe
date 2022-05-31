eada64a426812d83fa037e4e71ad557144908969
sarthak sachdeva <sarthak.sachdeva.73@gmail.com>
2022-05-31 21:04:53 +0530

    :ambulance: hotfix

--- src/components/home/timer/ticker.js
+++ src/components/home/timer/ticker.js
@@ -5,7 +5,7 @@ export const Ticker = ({ futureDate }) => {
   const { days, hours, minutes, seconds, isTimeUp } = useTicker(futureDate);
 
   return (
-    <div className="flex mx-auto my-auto p-8 gradientBox">
+    <div className="flex mx-auto my-auto sm:p-8 gradientBox">
       {isTimeUp ? (
         <div className="flex-1 text-center uppercase text-5xl font-semibold">
           Registrations are closed.
