4ebec25b085dc69934e813de6949c0e1cbc669f9
sarthak sachdeva <sarthak.sachdeva.73@gmail.com>
2022-06-06 17:21:39 +0530

    gvubh

--- src/components/home/sponsors.js
+++ src/components/home/sponsors.js
@@ -1,35 +1,6 @@
 // import { Marquee } from "react-easy-marquee";
 
 const Sponsors = () => {
-  const sponsors = [
-    {
-      name: "CodeDamn",
-      img: "http://levelupcollege.com/wp-content/uploads/2021/10/codedamn-logo-with-text-1.png",
-      description:
-        "Build projects, practice and learn to code from scratch - without leaving your browser.",
-      url: "https://codedamn.com/",
-    },
-    {
-      name: "Github Education",
-      img: "https://www.analyticsvidhya.com/wp-content/uploads/2015/07/github_logo.png",
-      description:
-        "GitHub Global Campus helps students, teachers, and schools access the tools and events they need to shape the next generation of software development.",
-      url: "https://education.github.com/",
-    },
-    {
-      name: "Jetbrains",
-      img: "https://images.yourstory.com/cs/images/brandSpotlight/jetbrains-variant-21559042489533.png",
-      description: "Essential tools for software developers and teams",
-      url: "https://www.jetbrains.com/",
-    },
-    {
-      name: "DigitalOcean",
-      img: "https://urbankenyans.com/wp-content/uploads/2018/01/digitalocean-logo.jpg",
-      description: "Simpler cloud.Happier devs.Better results.",
-      url: "https://www.digitalocean.com/",
-    },
-  ];
-
   return <></>;
 };
 
