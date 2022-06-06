a55599b5e0a6e42b040f4f6ddfb3f1425eb81c72
sarthak sachdeva <sarthak.sachdeva.73@gmail.com>
2022-06-06 17:20:19 +0530

    iy;vh

--- src/components/home/sponsors.js
+++ src/components/home/sponsors.js
@@ -1,4 +1,4 @@
-import { Marquee } from "react-easy-marquee";
+// import { Marquee } from "react-easy-marquee";
 
 const Sponsors = () => {
   const sponsors = [
@@ -30,42 +30,7 @@ const Sponsors = () => {
     },
   ];
 
-  return (
-    <section id="sponsors" className="m-8 mx-auto">
-      <h2 className="uppercase text-medium-turquoise text-3xl font-bold text-center">
-        Sponsors
-      </h2>
-
-      <Marquee
-        height="25rem"
-        width="85%"
-        pauseOnHover={true}
-        className="mx-auto my-8 p-8 gradientBox"
-        duration={Math.max(12000, sponsors.length * 1000)}
-        reverse
-      >
-        {sponsors.map((sponsor) => (
-          <a
-            key={sponsor.name}
-            href={sponsor.url}
-            target="_blank"
-            rel="noreferrer noopener"
-            className="p-4 text-base font-extrabold h-80 flex flex-col justify-between mx-12"
-          >
-            <figure className="flex flex-col mb-8 h-36 md:h-4/5">
-              <img
-                src={sponsor.img}
-                alt={`${sponsor.name} | Unahashed | GitHub Community SRM`}
-                className="mx-auto h-full"
-              />
-            </figure>
-
-            <p className="text-xl font-semibold">{sponsor.description}</p>
-          </a>
-        ))}
-      </Marquee>
-    </section>
-  );
+  return <></>;
 };
 
 export default Sponsors;
